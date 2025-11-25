e01a7d6dbdc210323846ec617300f731e4a8c8da
bouajilaprog <bouajilamedyessine@gmail.com>
2025-11-25 10:37:51 +0100

    style(layout): make all pages at least take the whole screen height

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,11 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <Navbar />
         {children}
       </body>
-    </html>
-  );
+    </html>);
 }
