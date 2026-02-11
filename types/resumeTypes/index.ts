7a44d59168bdcd64a3de7f2e177415c7ca7fc720
bouajilaprog <bouajilamedyessine@gmail.com>
2026-02-11 13:10:52 +0100

    feat: export all Zod schemas from resumeTypes index

--- types/resumeTypes/index.ts
+++ types/resumeTypes/index.ts
@@ -1,14 +1,22 @@
 // 
 export * from "./resumeItem.type";
+export * from "./resumeItem.schema";
 
 // sections
 export * from "./personalInfo.type";
+export * from "./personalInfo.schema";
 export * from "./education.type";
+export * from "./education.schema";
 export * from "./project.type";
+export * from "./project.schema";
 export * from "./skills.type";
+export * from "./skills.schema";
 export * from "./experience.type";
+export * from "./experience.schema";
 export * from "./certif.type";
+export * from "./certif.schema";
 export * from "./extraCurr.type";
+export * from "./extraCurr.schema";
 
 
 
